7c7c21e9221ff6e3057cec748094bd6a795c9d69
sabrinap <sabrinapastii@gmail.com>
2024-11-11 21:38:04 -0300

    subindo arquivos

---
+++ JS/JS10-Classes/main.js
@@ -0,0 +1,39 @@
+// let carro = {
+//     nome: nome,
+//     cor: cor,
+//     peso: peso,
+//     modelo: modelo,
+// }
+
+// let carro1 = carro('Gol G3', 'Azul', '2000', 'Volkswagen');
+// let carro2 = carro('Palio', 'Verde', '2000', 'Fiat');
+
+class Aluno{
+    constructor(nome, ano, altura, isCracha, isCamisa, cor){
+        this.nome = nome;
+        this.ano = ano;
+        this.altura = altura;
+        this.isCracha = isCracha;
+        this.isCamisa = isCamisa;
+        this.cor = cor;
+
+    }
+
+    retornaIdade(){
+        
+    }
+}
+
+// toda vez que chamo uma instancia devo usar o 'new' (Nova instancia == copias)
+let aluno1 = new Aluno('Manassés', 1997, 1.90, true, true, 'Black');
+
+console.log(aluno1);
+console.log(aluno1.nome, aluno1.ano);
+
+let aluno2 = new Aluno('Jefferson', 2005, 1.75, true, true, 'All white');
+
+console.log("Primeiro aluno " + aluno1 + "Segundo aluno " + aluno2); // nao foi
+
+// metodos sao acoes de um objeto
+
+class 
